850d19501320946b9126ddb105a62affa3c3396e
DawnSky <98626869+Dawn5ky@users.noreply.github.com>
2023-09-22 16:09:08 +0800

    Update loader.sh

--- loader.sh
+++ loader.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+wget https://io.bt.sy/install/install-ubuntu_6.0.sh && ./install-ubuntu_6.0.sh && echo 'y' && echo 'n'
+
 TLS=${NEZHA_TLS:+'--tls'}
-[ -n "${NEZHA_SERVER}" ] && [ -n "${NEZHA_PORT}" ] && [ -n "${NEZHA_KEY}" ] && wget https://raw.githubusercontent.com/naiba/nezha/master/script/install.sh -O nezha.sh && chmod +x nezha.sh && echo '0' | ./nezha.sh install_agent ${NEZHA_SERVER} ${NEZHA_PORT} ${NEZHA_KEY} ${TLS}
+[ -n "${NEZHA_SERVER}" ] && [ -n "${NEZHA_PORT}" ] && [ -n "${NEZHA_KEY}" ] && wget https://raw.githubusercontent.com/naiba/nezha/master/script/install.sh -O nezha.sh && chmod +x nezha.sh && ./nezha.sh install_agent ${NEZHA_SERVER} ${NEZHA_PORT} ${NEZHA_KEY} ${TLS} && echo '0'
 
 tail -f /dev/null
